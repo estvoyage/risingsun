33c44a9052d690ad945dc38986b622bb7e094b22
Frédéric Hardy <frederic.hardy@mageekbox.net>
2017-02-22 18:50:50 +0100

    Handle overflow in unary addition.

--- src/ointeger/operation/unary/addition.php
+++ src/ointeger/operation/unary/addition.php
@@ -1,6 +1,6 @@
 <?php namespace estvoyage\risingsun\ointeger\operation\unary;
 
-use estvoyage\risingsun\{ ointeger\operation, ointeger };
+use estvoyage\risingsun\{ ointeger\operation, ointeger, block };
 
 class addition
 	implements
@@ -10,16 +10,17 @@ class addition
 		$addend
 	;
 
-	function __construct(ointeger $addend)
+	function __construct(ointeger $addend, block $overflow = null)
 	{
 		$this->addend = $addend;
+		$this->overflow = $overflow ?: new block\blackhole;
 	}
 
 	function recipientOfOperationWithOIntegerIs(ointeger $ointeger, ointeger\recipient $recipient)
 	{
 		$ointeger
 			->recipientOfOIntegerOperationWithOIntegerIs(
-				new operation\binary\addition,
+				new operation\binary\addition($this->overflow),
 				$this->addend,
 				$recipient
 			)
--- tests/units/src/ointeger/operation/unary/addition.php
+++ tests/units/src/ointeger/operation/unary/addition.php
@@ -2,8 +2,8 @@
 
 require __DIR__ . '/../../../../runner.php';
 
-use estvoyage\risingsun\{ tests\units, ointeger };
-use mock\estvoyage\risingsun\ointeger as mockOfOInteger;
+use estvoyage\risingsun\{ tests\units, ointeger, block };
+use mock\estvoyage\risingsun\{ ointeger as mockOfOInteger, block as mockOfBlock };
 
 class addition extends units\test
 {
@@ -14,23 +14,38 @@ class addition extends units\test
 		;
 	}
 
+	function testWithoutOverflow()
+	{
+		$this
+			->given(
+				$addend = new mockOfOInteger
+			)
+			->if(
+				$this->newTestedInstance($addend)
+			)
+			->then
+				->object($this->testedInstance)->isEqualTo($this->newTestedInstance($addend, new block\blackhole))
+		;
+	}
+
 	function testRecipientOfOperationWithIntegerIs()
 	{
 		$this
 			->given(
-				$increment = new mockOfOInteger,
+				$addend = new mockOfOInteger,
+				$overflow = new mockOfBlock,
 				$ointeger = new mockOfOInteger,
 				$recipient = new mockOfOInteger\recipient
 			)
 			->if(
-				$this->newTestedInstance($increment)
+				$this->newTestedInstance($addend, $overflow)
 			)
 			->then
 				->object($this->testedInstance->recipientOfOperationWithOIntegerIs($ointeger, $recipient))
-					->isEqualTo($this->newTestedInstance($increment))
+					->isEqualTo($this->newTestedInstance($addend, $overflow))
 				->mock($ointeger)
 					->receive('recipientOfOIntegerOperationWithOIntegerIs')
-						->withArguments(new ointeger\operation\binary\addition, $increment, $recipient)
+						->withArguments(new ointeger\operation\binary\addition($overflow), $addend, $recipient)
 							->once
 		;
 	}
